44e7f2a0bb475ce49f15fde0435362df132088f0
BahodirL7 <bakhodirtursunovl7@gmail.com>
2024-12-30 13:30:46 +0900

    feat: SASS integration

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,22 +1,27 @@
 /* eslint-disable react/display-name */
-import { Stack } from "@mui/material";
 import Head from "next/head";
+import { Stack } from "@mui/material";
+
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
     return (
       <>
         <Head>
-          <title>Nestar</title>
+          <title>Nestar home</title>
         </Head>
+
         <Stack id="pc-wrap">
           <Stack sx={{ background: "#81c784" }}>Header Home</Stack>
+
           <Stack id={"main"}>
             <Component {...props} />
           </Stack>
+
           <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
         </Stack>
       </>
     );
   };
 };
+
 export default withLayoutMain;
